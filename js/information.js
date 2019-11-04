34248991934afba3cf43c5fb7ecbbd51079b39e1
Spycsh <757407490@qq.com>
2019-11-05 00:02:55 +0100

    mouse on events for buttons

--- js/information.js
+++ js/information.js
@@ -30,4 +30,81 @@ function disappearGPTHL(){
     document.getElementById("box4").style.display = "none";
 }
 
+function mouseOnCal(){
+    document.getElementById("calculateBtn").style.backgroundColor = "#fcf5d9";
+    document.getElementById("calculateBtn").style.backgroundImage = "linear-gradient(#fceaab, #fae185, #fcf5d9)";
+    document.getElementById("calculateBtn").style.borderColor = "#f2d359";
+}
 
+function mouseOutCal(){
+    document.getElementById("calculateBtn").style.backgroundImage = "linear-gradient(#ffef7c,#ffd86b,#ffe272)";
+    document.getElementById("calculateBtn").style.borderColor = "#c38e00";
+}
+
+// hover add button
+function mouseOnAdd1(){
+    document.getElementById("addBtn1").style.background = "url('css/addBtn2.png')"
+}
+
+function mouseOutAdd1(){
+    document.getElementById("addBtn1").style.background = "url('css/addBtn.png')"
+}
+
+function mouseOnAdd2(){
+    document.getElementById("addBtn2").style.background = "url('css/addBtn2.png')"
+}
+
+function mouseOutAdd2(){
+    document.getElementById("addBtn2").style.background = "url('css/addBtn.png')"
+}
+
+// hover delete button
+function mouseOnDelete1(){
+    document.getElementById("deleteBtn1").style.background = "url('css/deleteBtn2.png')"
+}
+
+function mouseOutDelete1(){
+    document.getElementById("deleteBtn1").style.background = "url('css/deleteBtn.png')"
+}
+
+function mouseOnDelete2(){
+    document.getElementById("deleteBtn2").style.background = "url('css/deleteBtn2.png')"
+}
+
+function mouseOutDelete2(){
+    document.getElementById("deleteBtn2").style.background = "url('css/deleteBtn.png')"
+}
+
+// hover edit button
+function mouseOnEdit1(){
+    document.getElementById("editBtn1").style.background = "url('css/editBtn2.png')"
+}
+
+function mouseOnEdit2(){
+    document.getElementById("editBtn2").style.background = "url('css/editBtn2.png')"
+}
+
+function mouseOutEdit1(){
+    document.getElementById("editBtn1").style.background = "url('css/editBtn.png')"
+}
+
+function mouseOutEdit2(){
+    document.getElementById("editBtn2").style.background = "url('css/editBtn.png')"
+}
+
+// hover reset button
+function mouseOnReset1(){
+    document.getElementById("resetBtn1").style.background = "url('css/resetBtn2.png')"
+}
+
+function mouseOnReset2(){
+    document.getElementById("resetBtn2").style.background = "url('css/resetBtn2.png')"
+}
+
+function mouseOutReset1(){
+    document.getElementById("resetBtn1").style.background = "url('css/resetBtn.png')"
+}
+
+function mouseOutReset2(){
+    document.getElementById("resetBtn2").style.background = "url('css/resetBtn.png')"
+}
